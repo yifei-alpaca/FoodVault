f0c67278f1fb0634d88bdf6ff7c91a21d2022120
sherryng-sktech <sherry.ng.2020@scis.smu.edu.sg>
2021-10-12 16:36:27 +0800

    Organize folders & Pushing Merchant Dashboard
    1. Created Raw Template for easy start
    2. Changed the navbar icon across all pages.
    3. Organized the img folder
    4. Pushing Merchant dashboard html page.  includes, css, js l in assets folder

---
+++ assets/js/merchant/merchant-dashboard-data.js
@@ -0,0 +1,389 @@
+data = {
+	initPickColor: function () {
+		$(".pick-class-label").click(function () {
+			var new_class = $(this).attr("new-class");
+			var old_class = $("#display-buttons").attr("data-class");
+			var display_div = $("#display-buttons");
+			if (display_div.length) {
+				var display_buttons = display_div.find(".btn");
+				display_buttons.removeClass(old_class);
+				display_buttons.addClass(new_class);
+				display_div.attr("data-class", new_class);
+			}
+		});
+	},
+
+	initDocChart: function () {
+		chartColor = "#FFFFFF";
+
+		// General configuration for the charts with Line gradientStroke
+		gradientChartOptionsConfiguration = {
+			maintainAspectRatio: false,
+			legend: {
+				display: false,
+			},
+			tooltips: {
+				bodySpacing: 4,
+				mode: "nearest",
+				intersect: 0,
+				position: "nearest",
+				xPadding: 10,
+				yPadding: 10,
+				caretPadding: 10,
+			},
+			responsive: true,
+			scales: {
+				yAxes: [
+					{
+						display: 0,
+						gridLines: 0,
+						ticks: {
+							display: false,
+						},
+						gridLines: {
+							zeroLineColor: "transparent",
+							drawTicks: false,
+							display: false,
+							drawBorder: false,
+						},
+					},
+				],
+				xAxes: [
+					{
+						display: 0,
+						gridLines: 0,
+						ticks: {
+							display: false,
+						},
+						gridLines: {
+							zeroLineColor: "transparent",
+							drawTicks: false,
+							display: false,
+							drawBorder: false,
+						},
+					},
+				],
+			},
+			layout: {
+				padding: {
+					left: 0,
+					right: 0,
+					top: 15,
+					bottom: 15,
+				},
+			},
+		};
+	},
+
+	initDashboardPageCharts: function () {
+		chartColor = "#FFFFFF";
+
+		// General configuration for the charts with Line gradientStroke
+		gradientChartOptionsConfiguration = {
+			maintainAspectRatio: false,
+			legend: {
+				display: false,
+			},
+			tooltips: {
+				bodySpacing: 4,
+				mode: "nearest",
+				intersect: 0,
+				position: "nearest",
+				xPadding: 10,
+				yPadding: 10,
+				caretPadding: 10,
+			},
+			responsive: 1,
+			scales: {
+				yAxes: [
+					{
+						display: 0,
+						gridLines: 0,
+						ticks: {
+							display: false,
+						},
+						gridLines: {
+							zeroLineColor: "transparent",
+							drawTicks: false,
+							display: false,
+							drawBorder: false,
+						},
+					},
+				],
+				xAxes: [
+					{
+						display: 0,
+						gridLines: 0,
+						ticks: {
+							display: false,
+						},
+						gridLines: {
+							zeroLineColor: "transparent",
+							drawTicks: false,
+							display: false,
+							drawBorder: false,
+						},
+					},
+				],
+			},
+			layout: {
+				padding: {
+					left: 0,
+					right: 0,
+					top: 15,
+					bottom: 15,
+				},
+			},
+		};
+
+		gradientChartOptionsConfigurationWithNumbersAndGrid = {
+			layout: {
+				padding: {
+					left: 20,
+					right: 20,
+					top: 0,
+					bottom: 0,
+				},
+			},
+			maintainAspectRatio: false,
+			tooltips: {
+				backgroundColor: "#fff",
+				titleFontColor: "#333",
+				bodyFontColor: "#666",
+				bodySpacing: 4,
+				xPadding: 12,
+				mode: "nearest",
+				intersect: 0,
+				position: "nearest",
+			},
+			legend: {
+				position: "bottom",
+				fillStyle: "#FFF",
+				display: false,
+			},
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							fontColor: "rgba(71,71,71,0.4)",
+							fontStyle: "bold",
+							beginAtZero: true,
+							maxTicksLimit: 5,
+							padding: 10,
+						},
+						gridLines: {
+							drawTicks: true,
+							drawBorder: false,
+							display: true,
+							color: "rgba(71,71,71,0.1)",
+							zeroLineColor: "transparent",
+						},
+					},
+				],
+				xAxes: [
+					{
+						gridLines: {
+							zeroLineColor: "transparent",
+							display: false,
+						},
+						ticks: {
+							padding: 10,
+							fontColor: "rgba(71,71,71,0.4)",
+							fontStyle: "bold",
+						},
+					},
+				],
+			},
+		};
+
+		var ctx = document.getElementById("bigDashboardChart").getContext("2d");
+
+		var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+		gradientStroke.addColorStop(0, "#9bb59a");
+		gradientStroke.addColorStop(1, chartColor);
+
+		var gradientFill = ctx.createLinearGradient(0, 200, 0, 50);
+		gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+		gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
+
+		var myChart = new Chart(ctx, {
+			type: "line",
+			data: {
+				labels: [
+					"JAN",
+					"FEB",
+					"MAR",
+					"APR",
+					"MAY",
+					"JUN",
+					"JUL",
+					"AUG",
+					"SEP",
+					"OCT",
+					"NOV",
+					"DEC",
+				],
+				datasets: [
+					{
+						// label: "Data",
+						borderColor: chartColor,
+						pointBorderColor: chartColor,
+						pointBackgroundColor: "#9bb59a",
+						pointHoverBackgroundColor: "#9bb59a",
+						pointHoverBorderColor: chartColor,
+						pointBorderWidth: 1,
+						pointHoverRadius: 7,
+						pointHoverBorderWidth: 2,
+						pointRadius: 5,
+						fill: true,
+						backgroundColor: gradientFill,
+						borderWidth: 2,
+						data: [
+							23540, 24940, 29890, 21650, 16584, 14870, 20640, 16430, 14720,
+							19340, 15780, 12320,
+						],
+					},
+				],
+			},
+			options: {
+				layout: {
+					padding: {
+						left: 20,
+						right: 20,
+						top: 0,
+						bottom: 0,
+					},
+				},
+				maintainAspectRatio: false,
+				tooltips: {
+					backgroundColor: "#fff",
+					titleFontColor: "#333",
+					bodyFontColor: "#666",
+					bodySpacing: 4,
+					xPadding: 12,
+					mode: "nearest",
+					intersect: 0,
+					position: "nearest",
+				},
+				legend: {
+					position: "bottom",
+					fillStyle: "#FFF",
+					display: false,
+				},
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								fontColor: "rgba(255,255,255,0.4)",
+								fontStyle: "bold",
+								beginAtZero: true,
+								maxTicksLimit: 5,
+								padding: 10,
+							},
+							gridLines: {
+								drawTicks: true,
+								drawBorder: false,
+								display: true,
+								color: "rgba(255,255,255,0.1)",
+								zeroLineColor: "transparent",
+							},
+						},
+					],
+					xAxes: [
+						{
+							gridLines: {
+								zeroLineColor: "transparent",
+								display: false,
+							},
+							ticks: {
+								padding: 10,
+								fontColor: "rgba(255,255,255,0.4)",
+								fontStyle: "bold",
+							},
+						},
+					],
+				},
+			},
+		});
+
+		ctx = document
+			.getElementById("lineChartExampleWithNumbersAndGrid")
+			.getContext("2d");
+
+		gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+		gradientStroke.addColorStop(0, "#2d8c28");
+		gradientStroke.addColorStop(1, chartColor);
+
+		gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+		gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+		gradientFill.addColorStop(1, hexToRGB("#2d8c28", 0.4));
+
+		myChart = new Chart(ctx, {
+			type: "line",
+			responsive: true,
+			data: {
+				labels: ["12pm", "3pm", "6pm", "9pm"],
+				datasets: [
+					{
+						label: "Total Receiver",
+						borderColor: "#2d8c28",
+						pointBorderColor: "#FFF",
+						pointBackgroundColor: "#2d8c28",
+						pointBorderWidth: 2,
+						pointHoverRadius: 4,
+						pointHoverBorderWidth: 1,
+						pointRadius: 4,
+						fill: true,
+						backgroundColor: gradientFill,
+						borderWidth: 2,
+						data: [232, 89, 120, 610],
+					},
+				],
+			},
+
+			options: gradientChartOptionsConfigurationWithNumbersAndGrid,
+		});
+
+		var e = document
+			.getElementById("barChartSimpleGradientsNumbers")
+			.getContext("2d");
+
+		gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+		gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+		gradientFill.addColorStop(1, hexToRGB("#2a648c", 0.6));
+
+		var a = {
+			type: "bar",
+			data: {
+				labels: [
+					"Monday",
+					"Tuesday",
+					"Wednesday",
+					"Thursday",
+					"Friday",
+					"Saturday",
+					"Sunday",
+				],
+				datasets: [
+					{
+						label: "Total Receiver",
+						backgroundColor: gradientFill,
+						borderColor: "#2a648c",
+						pointBorderColor: "#FFF",
+						pointBackgroundColor: "#2a648c",
+						pointBorderWidth: 2,
+						pointHoverRadius: 4,
+						pointHoverBorderWidth: 1,
+						pointRadius: 4,
+						fill: true,
+						borderWidth: 2,
+						data: [861, 1024, 856, 606, 1023, 1217, 1148],
+					},
+				],
+			},
+			options: gradientChartOptionsConfigurationWithNumbersAndGrid,
+		};
+
+		var viewsChart = new Chart(e, a);
+	},
+};
